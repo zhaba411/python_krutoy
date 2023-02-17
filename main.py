7ce49eea777019f522a6bb314e36134d57d2e265
student <zhaba.88888@gmail.com>
2023-02-17 16:28:10 +0300

    1234555555

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+def parol():
 
-
-print("gdashhaqweqwrqwtqwtfdafdafasfa")
-
-print("zekfas")
+    a = input("введите пароль (от 4 до 8 симв):  ")
+    b = input("повторите пароль:  ")
+    if (a == b) and ((len(a)<9) and (len(a)>3)):
+        return "пароли совп"
+    elif (a == b) and ((len(a)>8) or (len(a)<4)):
+        return "Пароль не подходит"
+    elif (a != b):
+        return "ТЫ ВАЩЕ ДУНДУК????"
+print (parol())
